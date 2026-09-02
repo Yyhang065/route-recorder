6f78b28656334b946e61e35baf6473f216fdef79
TheLight <yyhang065@gmail.com>
2026-09-02 22:13:55 +0800

    Add RouteRecorder class in main.mod.js

---
+++ 0.1.9/main.mod.js
@@ -0,0 +1,11 @@
+import {
+  PolyMod,
+  MixinType,
+} from "https://cdn.polymodloader.com/cb/PolyTrackMods/PolyModLoader/0.6.2/PolyTypes.js";
+
+class RouteRecorder extends PolyMod {
+  init = (pml) => {
+  };
+}
+
+export let polyMod = new RouteRecorder();
