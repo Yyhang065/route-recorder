f29f1af458d25017c675b61fd97e588e1590ab66
TheLight <yyhang065@gmail.com>
2026-09-20 17:03:25 +0800

    Refactor RouteRecorder by removing MixinType
    Removed MixinType usage and related code from RouteRecorder class.

--- 0.2.3/main.mod.js
+++ 0.2.3/main.mod.js
@@ -1,32 +1,11 @@
 import {
   PolyMod,
-  MixinType,
 } from "https://cdn.polymodloader.com/cb/PolyTrackMods/PolyModLoader/0.6.3/PolyTypes.js";
 
 class RouteRecorder extends PolyMod {
   init = (pml) => {
-    pml.registerChunkMixin("112", {
-      type: MixinType.INSERT,
-
-      token: "G.appendChild(C));",
-
-      func: `
-        {
-          const routeRecorderButton =
-            document.createElement("button");
-
-          routeRecorderButton.className = "button";
-          routeRecorderButton.textContent =
-            "Route Recorder";
-
-          G.appendChild(
-            routeRecorderButton
-          );
-        }
-      `,
-    });
+    console.log("[Route Recorder] 0.6.3 INIT WORKED");
   };
 }
 
-export let polyMod =
-  new RouteRecorder();
+export let polyMod = new RouteRecorder();
