d039ac981ced08066db727ef3946c0ba89a65538
TheLight <yyhang065@gmail.com>
2026-09-20 17:12:21 +0800

    Update main.mod.js

--- 0.2.3/main.mod.js
+++ 0.2.3/main.mod.js
@@ -3,6 +3,8 @@ import {
 } from "https://cdn.polymodloader.com/cb/PolyTrackMods/PolyModLoader/0.6.3/PolyTypes.js";
 
 class RouteRecorder extends PolyMod {
+  init = (pml) => {
+  };
 }
 
 export let polyMod = new RouteRecorder();
