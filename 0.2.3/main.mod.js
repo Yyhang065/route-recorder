54a8cbbaf11e6ff4a05d8f6fcc9bedbf62e962e4
TheLight <yyhang065@gmail.com>
2026-09-20 17:01:09 +0800

    Refactor Route Recorder button creation logic

--- 0.2.3/main.mod.js
+++ 0.2.3/main.mod.js
@@ -12,48 +12,15 @@ class RouteRecorder extends PolyMod {
 
       func: `
         {
-          console.log("[Route Recorder] 0.6.3 EDITOR MIXIN HIT");
+          const routeRecorderButton =
+            document.createElement("button");
 
-          const button = document.createElement("button");
+          routeRecorderButton.className = "button";
+          routeRecorderButton.textContent =
+            "Route Recorder";
 
-          button.className = "button";
-
-          let enabled = true;
-
-          const updateButton = () => {
-            const icon =
-              "data:image/svg+xml;charset=utf-8," +
-              encodeURIComponent(
-                '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">' +
-                  '<circle cx="12" cy="12" r="8" fill="none" stroke="white" stroke-width="2.5"/>' +
-                  '<circle cx="12" cy="12" r="4" fill="white"/>' +
-                "</svg>"
-              );
-
-            button.innerHTML =
-              '<img class="button-icon" src="' +
-              icon +
-              '"> ' +
-              (enabled ? "Enabled" : "Disabled");
-          };
-
-          button.addEventListener("click", () => {
-            enabled = !enabled;
-
-            updateButton();
-
-            console.log(
-              "[Route Recorder] Button:",
-              enabled ? "Enabled" : "Disabled"
-            );
-          });
-
-          G.appendChild(button);
-
-          updateButton();
-
-          console.log(
-            "[Route Recorder] 0.6.3 BUTTON CREATED"
+          G.appendChild(
+            routeRecorderButton
           );
         }
       `,
@@ -61,4 +28,5 @@ class RouteRecorder extends PolyMod {
   };
 }
 
-export let polyMod = new RouteRecorder();
+export let polyMod =
+  new RouteRecorder();
