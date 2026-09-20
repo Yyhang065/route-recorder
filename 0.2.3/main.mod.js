a7fa0b3283cc81ce831576055cfdb7128624bf8b
TheLight <yyhang065@gmail.com>
2026-09-20 17:08:21 +0800

    Refactor RouteRecorder initialization method

--- 0.2.3/main.mod.js
+++ 0.2.3/main.mod.js
@@ -3,9 +3,6 @@ import {
 } from "https://cdn.polymodloader.com/cb/PolyTrackMods/PolyModLoader/0.6.3/PolyTypes.js";
 
 class RouteRecorder extends PolyMod {
-  init = (pml) => {
-    console.log("[Route Recorder] 0.6.3 INIT WORKED");
-  };
 }
 
 export let polyMod = new RouteRecorder();
