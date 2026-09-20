b54ef05abf5125c95c67d47ecfb3078432aee5a4
TheLight <yyhang065@gmail.com>
2026-09-20 17:29:20 +0800

    Add chunk mixin registration to RouteRecorder

--- 0.2.3/main.mod.js
+++ 0.2.3/main.mod.js
@@ -1,9 +1,18 @@
 import {
   PolyMod,
+  MixinType,
 } from "https://cdn.polymodloader.com/cb/PolyTrackMods/PolyModLoader/0.6.3/PolyTypes.js";
 
 class RouteRecorder extends PolyMod {
   init = (pml) => {
+    pml.registerChunkMixin("112", {
+      type: MixinType.INSERT,
+      token: "G.appendChild(C));",
+      func: `
+        {
+        }
+      `,
+    });
   };
 }
 
